237a2dc00d48dd76d34d238a813dfe0f87843b23
InQueueq <dimadorko@gmail.com>
2020-03-12 23:22:27 +0200

    edited scenes

--- WarGame/Assets/Scripts/TurnSystem.cs
+++ WarGame/Assets/Scripts/TurnSystem.cs
@@ -12,10 +12,6 @@ public class TurnSystem : MonoBehaviour
     public CameraFollow cam;
     void Start()
     {
-
-        //cam.cameraPos = player1.transform.position;
-        //player1.GetComponent<CameraMover>().inputX = player1.transform.position.x;
-        //player1.GetComponent<CameraMover>().inputZ = player1.transform.position.y;
         player2.GetComponent<Player>().stamina = 0;
         TurnPass.onClick.AddListener(NextTurn);
     }
@@ -34,15 +30,11 @@ public class TurnSystem : MonoBehaviour
     {
         if(turnCounter % 2 != 0)
         {
-            // player1.GetComponent<CameraFollow>().cameraPos = player1.transform.position;
-            //cam.cameraPos = player1.transform.position;
             player1.GetComponent<Player>().stamina = 10;
             player2.GetComponent<Player>().stamina = 0;
         }
         else
         {
-            //player2.GetComponent<CameraFollow>().cameraPos = player2.transform.position;
-            //cam.cameraPos = player2.transform.position;
             player2.GetComponent<Player>().stamina = 10;
             player1.GetComponent<Player>().stamina = 0;
         }
